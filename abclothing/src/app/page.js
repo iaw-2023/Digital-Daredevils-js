33f5bc0257c1e3dac79317a2108fb31e51eb0b6a
albanomazzino <albimazzino@gmail.com>
2023-05-27 00:09:05 -0300

    App now in strict mode for debugging purposes

--- abclothing/src/app/page.js
+++ abclothing/src/app/page.js
@@ -1,7 +1,12 @@
 import dynamic from 'next/dynamic';
+import React from 'react';
 // Load the App component dynamically
 const App = dynamic(() => import('./pages/App'), { ssr : false});
 
 export default function Home() {
-  return <App />;
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
