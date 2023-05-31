2a1668966e0043de5e60c495526a56928dd592ff
albanomazzino <albimazzino@gmail.com>
2023-05-30 23:14:48 -0300

    Updated sidebar visuals

--- abclothing/src/app/components/Sidebar.jsx
+++ abclothing/src/app/components/Sidebar.jsx
@@ -1,63 +1,54 @@
-"use client";
+  import {
+    Box,
+    Button,
+    Drawer,
+    DrawerBody,
+    DrawerCloseButton,
+    DrawerContent,
+    DrawerHeader,
+    DrawerOverlay,
+    Flex,
+    Image,
+    Text,
+    useDisclosure
+  } from "@chakra-ui/react";
+  import React, { useState, useEffect } from "react";
+  import { Link } from "react-router-dom";
+  import { CATEGORIAS } from "./Categorias/Categorias";
+  import { GiHamburgerMenu } from "react-icons/gi";
+  import "./Sidebar.css";
 
-import {
-  Box,
-  Button,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerHeader,
-  DrawerOverlay,
-  Flex,
-  Image,
-  Text,
-  useDisclosure
-} from "@chakra-ui/react";
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { Categoria } from "./Categorias/Categoria";
-import { CATEGORIAS } from "./Categorias/Categorias";
-import { GiHamburgerMenu } from "react-icons/gi";
-import "./Sidebar.css";
+  function SideBar() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [categorias, setCategorias] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-function SideBar() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [categorias, setCategorias] = useState(null);
-  const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      const fetchCategorias = async () => {
+        try {
+          setLoading(true);
+          const response = await CATEGORIAS();
+          setCategorias(response.data);
+          setLoading(false);
+        } catch (error) {
+          setLoading(false);
+          console.log("Error fetching categorias:", error);
+        }
+      };
 
-  useEffect(() => {
-    const fetchCategorias = async () => {
-      try {
-        setLoading(true);
-        const response = await CATEGORIAS();
-        setCategorias(response.data);
-        setLoading(false);
-      } catch (error) {
-        setLoading(false);
-        console.log("Error fetching categorias:", error);
-      }
-    };
+      fetchCategorias();
+    }, []);
 
-    fetchCategorias();
-  }, []);
-  
-  
-  if (loading){
-    <h1>Loading in HomeMenu.jsx...</h1>
-  }
-  else{
+    if (loading) {
+      return <h1>Loading in Sidebar...</h1>;
+    }
+
     return (
       <>
-        <Button
-          onClick={onOpen}
-          colorScheme="yellow"
-          size="md"
-          _hover={{ bg: "pink" }}
-        >
+        <Button onClick={onOpen} colorScheme="yellow" size="md" _hover={{ bg: "pink" }}>
           <GiHamburgerMenu size={"18px"} />
         </Button>
-  
+
         <Drawer onClose={onClose} isOpen={isOpen}>
           <DrawerOverlay />
           <DrawerContent>
@@ -76,77 +67,38 @@ function SideBar() {
                   height={{ base: "3rem", md: "100%" }}
                 />
               </Box>
-  
-              <Flex
-                justify="center"
-                pl="1rem"
-                gap="5"
-                flexDir={"column"}
-                mx="2rem"
-                mt="2rem"
-              >
-                <Link to="/">
-                  <Text
-                    textAlign={"center"}
-                    fontSize={"1.5rem"}
-                    transition="0.5s ease"
-                    _hover={{
-                      borderBottomWidth: "4px",
-                      borderBottomColor: "#f89f17",
-                      color: "pink.600",
-                      fontSize: "1.7rem",
-                      webkitTransform: "scale(1.04)",
-                      msTransform: "scale(1.02)",
-                      transform: "scale(1.02)",
-                      transition: " 0.5s ease",
-                    }}
-                  >
-                    Home
+
+              <Flex justify="center" pl="1rem" gap="5" flexDir="column" mx="2rem" mt="2rem">
+                <Link to="/" className="sidebar-categoria-link">
+                  <Text textAlign="center" fontSize="1.5rem" >
+                    Shop
                   </Text>
                 </Link>
-                <Link to="#">
-                  <div classname ="categoria">
-                    {Object.values(categorias).map((categoria) => (
-                        <Categoria data={categoria} key={categoria.id} />
+
+                <div className="sidebar-categorias">
+                  {categorias &&
+                    Object.values(categorias).map((categoria) => (
+                      <Link
+                        to={`/categorias/${categoria.id}/${categoria.nombre}`}
+                        key={categoria.id}
+                        className="sidebar-categoria-link"
+                      >
+                        <Text textAlign="center" fontSize="1.5rem">
+                          {categoria.nombre}
+                        </Text>
+                      </Link>
                     ))}
-                  </div>
-                </Link>
-                <Link to="/carrito">
-                  <Text
-                    textAlign={"center"}
-                    fontSize={"1.5rem"}
-                    transition="0.5s ease"
-                    _hover={{
-                      borderBottomWidth: "4px",
-                      borderBottomColor: "#f89f17",
-                      color: "pink.600",
-                      fontSize: "1.7rem",
-                      webkitTransform: "scale(1.04)",
-                      msTransform: "scale(1.02)",
-                      transform: "scale(1.02)",
-                      transition: " 0.5s ease",
-                    }}
-                  >
-                    Your Cart
+                </div>
+
+                <Link to="/carrito" className="sidebar-categoria-link">
+                  <Text textAlign="center" fontSize="1.5rem" >
+                    Carrito
                   </Text>
                 </Link>
-                <Link to="#">
-                  <Text
-                    textAlign={"center"}
-                    fontSize={"1.5rem"}
-                    transition="0.5s ease"
-                    _hover={{
-                      borderBottomWidth: "4px",
-                      borderBottomColor: "#f89f17",
-                      color: "pink.600",
-                      fontSize: "1.7rem",
-                      webkitTransform: "scale(1.04)",
-                      msTransform: "scale(1.02)",
-                      transform: "scale(1.02)",
-                      transition: " 0.5s ease",
-                    }}
-                  >
-                    Profile
+
+                <Link to="#" className="sidebar-categoria-link">
+                  <Text textAlign="center" fontSize="1.5rem" >
+                    Perfil
                   </Text>
                 </Link>
               </Flex>
@@ -156,6 +108,5 @@ function SideBar() {
       </>
     );
   }
-  
-}
-export default SideBar;
+
+  export default SideBar;
