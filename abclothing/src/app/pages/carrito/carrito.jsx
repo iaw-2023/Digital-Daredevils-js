b5b52ac7ee5c98c6db8fe08bdc0bcc84d6e7079d
albanomazzino <albimazzino@gmail.com>
2023-05-31 19:58:56 -0300

    carrito is now indentado, email request feature 1/2

--- abclothing/src/app/pages/carrito/carrito.jsx
+++ abclothing/src/app/pages/carrito/carrito.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useContext, useState, useEffect } from "react";
 import { ShopContext } from "../../context/shop-context";
 import { ItemCarrito } from "./itemCarrito";
@@ -15,7 +13,7 @@ export const Carrito = () => {
   const [totalCarritoLoading, setTotalCarritoLoading] = useState(true);
 
   const [showModal, setShowModal] = useState(false);
-  const [text, setText] = useState('')
+  const [text, setText] = useState('');
   const handleTextChange = (event) => {
     setText(event.target.value);
   };
@@ -40,10 +38,10 @@ export const Carrito = () => {
   } else {
     return (
       <div className="carrito">
-          <div className="title">
-            Prendas en el carrito
-          </div>
-            {!productosCarrito ? null :
+        <div className="title">
+          Prendas en el carrito
+        </div>
+        {!productosCarrito ? null :
           Object.values(productosCarrito).map((producto) => {
             if (producto.amount !== 0) {
               return <ItemCarrito data={producto} key={producto.id} />;
@@ -51,68 +49,62 @@ export const Carrito = () => {
             return null;
           })}
 
-          {totalCarrito > 0 ? (
-              <div className="checkout">
-                <div className="subtotal">
-                  <h2> Subtotal: ${Math.floor(totalCarrito).toLocaleString()}
-                  <sup className="fractional-part">{totalCarrito.toLocaleString().slice(-2)}</sup>
-                  </h2>
+        {totalCarrito > 0 ? (
+          <div className="checkout">
+            <div className="subtotal">
+              <h2>
+                Subtotal: ${Math.floor(totalCarrito).toLocaleString()}
+                <sup className="fractional-part">{totalCarrito.toLocaleString().slice(-2)}</sup>
+              </h2>
+            </div>
+            <div className="buttons">
+              <button onClick={() => navigate("/")}>Seguir comprando</button>
+              <button onClick={() => setShowModal(true)}>Checkout</button>
+            </div>
+          </div>
+        ) : (
+          <h1>Tu carrito está vacío</h1>
+        )}
+
+        {showModal ? (
+          <div className="modal-background">
+            <div className="mensaje" style={{ position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
+              <div className="pedido">Ingrese correo para la compra</div>
+              <div>
+                <div>
+                  <textarea
+                    value={text}
+                    onChange={handleTextChange}
+                    placeholder="e.g.: riverplate@gmail.com"
+                    rows={1}
+                    cols={50}
+                    style={{
+                      border: '1px solid #ccc',
+                      borderRadius: '4px',
+                      padding: '8px',
+                      resize: 'vertical',
+                      fontSize: '14px',
+                      width: '100%',
+                      height: '50%'
+                    }}
+                  />
                 </div>
-                <div className="buttons">
-                  <button onClick={() => navigate("/")}>Seguir comprando</button>
+                <div className="close">
+                  <button onClick={() => setShowModal(false)}>Cerrar</button>
                   <button
-                      onClick={() => setShowModal(true)}
+                    onClick={() => {
+                      checkout(text);
+                    }}
+                    style={{ marginTop: '8px' }}
                   >
-                      Checkout 
+                    Enviar pedido
                   </button>
                 </div>
               </div>
-            ) : (
-              <h1>Tu carrito está vacío</h1>
-            )}
-            {showModal ? (
-              <div className="mensaje">
-                        <div className=" pedido ">
-                          se necesita E-mail de confirmacion
-                        </div>
-                        <div>
-                                <div>
-                                  <textarea
-                                    value={text}
-                                    onChange={handleTextChange}
-                                    placeholder="Ingresa tu texto aquí"
-                                    rows={1}
-                                    cols={50}
-                                    style={{
-                                      border: '1px solid #ccc',
-                                      borderRadius: '4px',
-                                      padding: '8px',
-                                      resize: 'vertical',
-                                      fontSize: '14px',
-                                      width: "100%",
-                                      height: "50%"
-                                    }}
-                                  />
-                                </div>
-                      
-                                <div className="close">
-                                      <button
-                                        onClick={() => setShowModal(false)}
-                                      >
-                                        Cerrar
-                                      </button>
-                                      <button 
-                                        onClick={() => {
-                                          checkout(text);
-                                        }}
-                                        style={{ marginTop: '8px' }}>
-                                          Enviar Pedido
-                                      </button>
-                                </div>
-                        </div>
-              </div>
-            ) : null}
-    </div>
+            </div>
+          </div>
+        ) : null}
+      </div>
     );
   }
 };
