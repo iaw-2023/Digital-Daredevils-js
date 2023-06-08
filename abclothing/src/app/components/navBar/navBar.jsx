82139d9070b835b5372549636acf46f3341834d9
albanomazzino <albimazzino@gmail.com>
2023-06-08 17:16:10 -0300

    Auth0 -  User authentication (beta)

--- abclothing/src/app/components/navBar/navBar.jsx
+++ abclothing/src/app/components/navBar/navBar.jsx
@@ -15,17 +15,22 @@ import {
   Menu
 } from "@chakra-ui/react";
 import { BsBagHeart, BsBag, BsEmojiSmile, BsPerson } from "react-icons/bs";
+import { FaPersonThroughWindow } from "react-icons/fa";
 import HomeMenu from "../menu/Menu";
 import SearchBar from "../searchBar/searchBar";
 import SideBar from "../sideBar/Sidebar";
 import { ShopContext } from "../../context/shop-context";
 import LoadingSpinner from "../loadingSpinner/LoadingSpinner";
 import { showFailureMessage } from "../alerts/alerts";
+import LoginButton from "../login/login";
+import LogoutButton from "../login/logout";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./navBar.css";
 
 export const Navbar = () => {
   const navigate = useNavigate(); 
   const { productosCarrito, email } = useContext(ShopContext);
+  const { isAuthenticated } = useAuth0();
 
   const handleMisPedidosAttempt = () =>{
     if (!email) {
@@ -89,17 +94,24 @@ export const Navbar = () => {
                      <BsPerson fontSize={"1.3rem"} />
                   </MenuButton>
                   <MenuList>
-                    <MenuGroup title="Perfil" color ="teal.500">
-                      <MenuItem icon={<BsEmojiSmile size={20} />}
-                        style={{ pointerEvents: "none", cursor: "default" }}
-                        color="beige.400"
-                      >
-                        Hola, {email ? email : "crack"}!
-                      </MenuItem>
-                      <MenuItem icon={<BsBagHeart size={20} />} onClick={() => handleMisPedidosAttempt()} color="beige.400">
-                        Mis pedidos
-                      </MenuItem>
-                    </MenuGroup>
+                    {isAuthenticated ?
+                      <MenuGroup title="Perfil" color ="teal.500">
+                        <MenuItem icon={<BsEmojiSmile size={20} />}
+                          style={{ pointerEvents: "none", cursor: "default" }}
+                          color="beige.400"
+                        >
+                          Hola, {email ? email : "crack"}!
+                        </MenuItem>
+                        <MenuItem icon={<BsBagHeart size={20} />} onClick={() => handleMisPedidosAttempt()} color="beige.400">
+                          Mis pedidos
+                        </MenuItem>
+                        <MenuItem icon={<FaPersonThroughWindow size={20} />} color="beige.400">
+                          <LogoutButton/>
+                        </MenuItem>
+                      </MenuGroup>
+                    : 
+                      <LoginButton/>
+                    }
                   </MenuList>
                 </Menu>
               </Popover>
--- abclothing/src/app/pages/App.js
+++ abclothing/src/app/pages/App.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import { Navbar } from '../components/navBar/navBar';
 import { Shop } from './shop/shop';
 import { Contacto } from './contacto/contacto';
@@ -14,48 +14,62 @@ import { ShopSearch } from './shop/shopSearch';
 import { DetallesPedido } from './detallesPedido/DetallesPedidoView';
 import { MisPedidos } from './misPedidos/misPedidos';
 import { ToastContainer } from 'react-toastify';
+import { Auth0Provider } from '@auth0/auth0-react';
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
-
+const domain = "dev-isj4pwd0ownme8e8.us.auth0.com" // aca deberia tomarlo del .env cuando se digne a tomarlo
+const clientId = "B9scjNlVuJcq5rlgxdLfxCnFzcFQAxxX"; // aca deberia tomarlo del .env cuando se digne a tomarlo
 function App() {
+
+  const handleRedirectCallback = (appState) => {
+    const navigate = useNavigate();
+    const returnUrl = appState?.returnTo || '/';
+    navigate(returnUrl);
+  };
   return (
     <>
-      <div className="App">
-        <Router>
-          <ChakraProvider>
-            <ToastContainer
-              position="bottom-left"
-              autoClose={3000}
-              hideProgressBar={false}
-              newestOnTop={true}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="light"
-            />
-            <ShopContextProvider>
-              <Navbar />
-              <Routes>
-                <Route path="/" element={<Shop />} />
-                <Route path="/shopSearch" element={<ShopSearch />} />
-                <Route path="/contacto" element={<Contacto />} />
-                <Route path="/carrito" element={<Carrito />} />
-                <Route path="/misPedidos" element={<MisPedidos />} />
-                <Route path="/productos/:id" element={<ProductDetails />} />
-                <Route path="/categorias/:id/:nombre" element={<ShopCategoria />} />
-                <Route path="/detallesPedido/:id" element={<DetallesPedido />} />
-              </Routes>
-              <footer>
-                <Link to="https://github.com/iaw-2023/Digital-Daredevils-js/">
-                  <p>Created by Digital Daredevils ®</p>
-                </Link>
-              </footer>
-            </ShopContextProvider>
-          </ChakraProvider>
-        </Router>
-      </div>
+      <Router>
+        <Auth0Provider domain={domain} clientId={clientId}  onRedirectCallback={handleRedirectCallback}
+          authorizationParams={{
+            redirect_uri: "https://localhost:3000"
+          }}
+        >
+          <div className="App">
+            <ChakraProvider>
+              <ToastContainer
+                position="bottom-left"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={true}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+              />
+              <ShopContextProvider>
+                <Navbar />
+                <Routes>
+                  <Route path="/" element={<Shop />} />
+                  <Route path="/shopSearch" element={<ShopSearch />} />
+                  <Route path="/contacto" element={<Contacto />} />
+                  <Route path="/carrito" element={<Carrito />} />
+                  <Route path="/misPedidos" element={<MisPedidos />} />
+                  <Route path="/productos/:id" element={<ProductDetails />} />
+                  <Route path="/categorias/:id/:nombre" element={<ShopCategoria />} />
+                  <Route path="/detallesPedido/:id" element={<DetallesPedido />} />
+                </Routes>
+                <footer>
+                  <Link to="https://github.com/iaw-2023/Digital-Daredevils-js/">
+                    <p>Created by Digital Daredevils ®</p>
+                  </Link>
+                </footer>
+              </ShopContextProvider>
+            </ChakraProvider>
+          </div>
+        </Auth0Provider>
+      </Router>
     </>
   );
 }
