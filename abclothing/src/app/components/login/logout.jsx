d513fc30871cd226608a7ceb759233d9760757e2
albanomazzino <albimazzino@gmail.com>
2023-06-21 11:54:21 -0300

    Logout redirect

--- abclothing/src/app/components/login/logout.jsx
+++ abclothing/src/app/components/login/logout.jsx
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () => {
   const { logout, isAuthenticated } = useAuth0();
-    return isAuthenticated && <button onClick={() => logout()}>Log Out</button>;
+    return isAuthenticated && <button onClick={() => logout({returnTo:'https://digital-daredevils-js-albanomazzino-digitaldaredevils.vercel.app/'})}>Log Out</button>;
 };
 
 export default LogoutButton;
